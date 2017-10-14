87b4a14b31555558ee1996652128728352a12819
Manuel Rodriguez <manrodri92@gmail.com>
2017-10-14 03:08:25 -0400

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCN4xmpNe_zvJ8fWnINA_F0AtA8_-RsIfY",
+    authDomain: "polyprogressive-84031.firebaseapp.com",
+    databaseURL: "https://polyprogressive-84031.firebaseio.com",
+    projectId: "polyprogressive-84031",
+    storageBucket: "polyprogressive-84031.appspot.com",
+    messagingSenderId: "563888814774"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
